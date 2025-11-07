850d116eb2a630863246952aeb98cf3971dc208e
pullavaras <linneagodunov@protonmail.com>
2025-11-07 11:07:21 +0200

    -

---
+++ mod-10/classes/hissi.py
@@ -0,0 +1,11 @@
+class Hissi:
+    
+    def __init__(self, alin_kerros, ylin_kerros):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+    
+    def siirry_kerrokseen(self):
+        
+    def kerros_ylös(self):
+        
+    def kerros_alas(self):
