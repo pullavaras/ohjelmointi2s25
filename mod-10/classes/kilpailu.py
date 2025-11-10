d734ec8922934332b08c51833942cdda0573a42b
pullavaras <linneagodunov@protonmail.com>
2025-11-10 20:00:59 +0200

    -

--- mod-10/classes/kilpailu.py
+++ mod-10/classes/kilpailu.py
@@ -2,3 +2,10 @@
 
 class Kilpailu:
 
+    def __init__(self, nimi, km, autolista):
+        self.nimi = nimi
+        self.km = km
+        self.autolista = autolista
+
+    def tunti_kuluu(self):
+        for auto in self.autolista:
