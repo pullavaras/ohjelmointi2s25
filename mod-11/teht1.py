3f3b1dd8b60578e6bb7a5dba8b232fc253d97684
pullavaras <linneagodunov@protonmail.com>
2025-11-11 22:18:04 +0200

    -

---
+++ mod-11/teht1.py
@@ -0,0 +1,36 @@
+class Julkaisu:
+
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(f"Julkaisun: {self.nimi}")
+
+class Kirja(Julkaisu):
+
+    def __init__(self, nimi, kirjoittaja, sivum):
+        self.kirjoittaja = kirjoittaja
+        self.sivum = sivum
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Kirjailija on {self.kirjoittaja} ja sivumäärä on {self.sivum}.\n")
+
+
+class Lehti(Julkaisu):
+
+    def __init__(self, nimi, päätoimittjaja):
+        self.päätoimittaja = päätoimittjaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Päätoimittaja on {self.päätoimittaja}.")
+
+kirja = Kirja("Hytti numero 6", "Rosa Liksom", 200)
+lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+
+kirja.tulosta_tiedot()
+lehti.tulosta_tiedot()
+
