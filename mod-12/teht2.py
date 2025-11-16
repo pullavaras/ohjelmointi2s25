a08a00e650c01ca06c3e9c00cdc3617645eeea8e
pullavaras <linneagodunov@protonmail.com>
2025-11-16 15:54:15 +0200

    -

--- mod-12/teht2.py
+++ mod-12/teht2.py
@@ -1,5 +1,25 @@
 import json
 import requests
 
-paikka = print("Anna paikkakunta: ")
+paikka = input("Anna paikkakunta, ja saat säätiedot: ")
+apiavain = "###"
+pyyntö = f"https://api.openweathermap.org/data/2.5/weather?q={paikka}&appid={apiavain}&units=metric&lang=fi"
 
+try:
+
+    vastaus = requests.get(pyyntö)
+    if vastaus.status_code == 200:
+        json_vastaus = vastaus.json()
+
+        kaupunki = json_vastaus['name']
+        sää = json_vastaus['weather'][0]['description']
+        lämpötila = json_vastaus['main']['temp']
+
+        print(f"Sää paikassa {kaupunki} on {sää}.")
+        print(f"Lämpötila on {lämpötila:.1f} astetta C.")
+
+    else:
+        print("Haku epäonnistui.")
+
+except requests.exceptions.RequestException:
+    print("Hakua ei voitu suorittaa.")
